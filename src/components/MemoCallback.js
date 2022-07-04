9feb8404ad57da5e15084196e3622e7b073b04c6
Naim Hossain <najmulspark@gmail.com>
2022-07-04 21:17:29 +0600

    first commit

--- src/components/MemoCallback.js
+++ src/components/MemoCallback.js
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import { useCallback, useMemo, useState } from 'react';
 import ShowCount from "./ShowCount";
 import Title from "./Title";
 
@@ -14,18 +15,24 @@ function MemoCallBakc() {
     setCount2((prevCount) => prevCount + 5);
   }, []);
 
-  const isEvenOrOdd = useMemo(() => {
+  // const isEvenOrOdd = useMemo(() => {
+  //   let i = 0;
+  //   while (i < 1000000000) i += 1; // costly operation
+  //   return count1 % 2 === 0;
+  // }, [count1]);
+
+  const isEvenOrOdd = () => {
     let i = 0;
     while (i < 1000000000) i += 1; // costly operation
     return count1 % 2 === 0;
-  }, [count1]);
+  };
 
   return (
     <>
       <div>
         <Title />
         <ShowCount count={count1} title="Counter 1" />
-        <span>{isEvenOrOdd ? "Even" : "Odd"}</span>
+        <span>{isEvenOrOdd() ? "Even" : "Odd"}</span>
         <Button handleClick={incrementByOne}>Increment by one</Button>
         <hr />
         <ShowCount count={count2} title="Counter 2" />
